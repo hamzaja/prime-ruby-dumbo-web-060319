2bb7ec35d2d1a9d4c850bfb814e855f3284b3551
HAMZA Javed <hamzajaved71@gmail.com>
2019-05-26 03:26:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 def prime?(a)
   for num in 2..(a-1)
-    if a % num == 0 || num != 2
-      return FALSE 
-    else return TRUE
+    if a % num == 0 
+      return true 
+    else return false
     end
   end
 end
